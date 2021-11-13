2edf128e15cfb73e16b47b1e5c8c0c51f68d901b
hayashi-shio-lab <hayashi@shio-lab.com>
2021-11-13 13:55:13 +0900

    Increase versatility of highresnowc handler.

--- terracotta/handlers/highresnowc.py
+++ terracotta/handlers/highresnowc.py
@@ -39,13 +39,23 @@ def highresnowc(keys: Union[Sequence[str], Mapping[str, str]],
     tile_x, tile_y, tile_z = tile_xyz
     tile_width, tile_height = tile_size
     tile_data = np.zeros((tile_width, tile_height))
+    section_x_idx = driver.key_names.index('section_x')
+    section_y_idx = driver.key_names.index('section_y')
+    resolution_idx = driver.key_names.index('resolution')
+    sections_x = keys[section_x_idx].split(',')
+    sections_y = keys[section_y_idx].split(',')
+    resolutions = [int(r) for r in keys[resolution_idx].split(',')]
+    max_resolution = max(resolutions)
     with driver.connect():
-        for i in [0, 1]:
-            keys[3] = str(i)
-            for j in [0, 1]:
-                keys[4] = str(j)
-                for res in [160, 40]:
-                    keys[1] = str(res)
+        for x in sections_x:
+            keys[section_x_idx] = x
+            for y in sections_y:
+                keys[section_y_idx] = y
+                for res in resolutions:
+                    if res != max_resolution and max_resolution % res != 0:
+                        # Unsupported if max_resolution can not be divided by res.
+                        continue
+                    keys[resolution_idx] = str(res)
                     metadata = driver.get_metadata(keys)
                     wgs_bounds = metadata['bounds']
                     if not xyz.tile_exists(wgs_bounds, tile_x, tile_y, tile_z):
@@ -54,8 +64,9 @@ def highresnowc(keys: Union[Sequence[str], Mapping[str, str]],
                         driver, keys, tile_xyz,
                         tile_size=tile_size, preserve_values=preserve_values
                     )
-                    if res == 40:
-                        tdata.repeat(2, axis=0).repeat(2, axis=1)
+                    if res != max_resolution:
+                        scale = max_resolution / res
+                        tdata.repeat(scale, axis=0).repeat(scale, axis=1)
                     tile_data = np.maximum(tile_data, tdata)
     '''
         uint8  :    uint16   (precipitation      mm/h)
