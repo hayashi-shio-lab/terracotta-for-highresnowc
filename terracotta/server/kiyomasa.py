6a48f4482f7c97412892ef73a648c6a8e55869ef
hayashi-shio-lab <hayashi@shio-lab.com>
2023-02-27 16:33:34 +0900

    Fix handler existence check.

--- terracotta/server/kiyomasa.py
+++ terracotta/server/kiyomasa.py
@@ -99,21 +99,9 @@ def _get_kiyomasa_image(keys: str, tile_xyz: Tuple[int, int, int] = None) -> Res
     options = option_schema.load(request.args)
 
     data_kind = parsed_keys[0]
-    if data_kind not in (handlers.Pri60lv,
-                         handlers.Pphw10,
-                         handlers.Plts10,
-                         handlers.CWM_height,
-                         handlers.CWM_period,
-                         handlers.CWM_direction,
-                         handlers.MSM_temp,
-                         handlers.MSM_rh,
-                         handlers.HDW_temp,
-                         handlers.HDW_rh,
-                         handlers.HDW_precip,
-                         handlers.HDW_wind_speed,
-                         handlers.HDW_wind_dir,
-                        ):
-        raise exceptions.InvalidKeyError(f"unknown data kind '{data_kind}'.")
-    handler = getattr(handlers, data_kind)
+    try:
+        handler = getattr(handlers, data_kind)
+    except AttributeError as e:
+        raise exceptions.InvalidKeyError(e)
     image = handler(parsed_keys, tile_xyz=tile_xyz, **options)
     return send_file(image, mimetype='image/png')
